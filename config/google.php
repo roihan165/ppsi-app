5d02d0584302d9fb9422b76ff7800336ac76519b
Roy <muhammadroihan165@gmail.com>
2025-07-08 16:04:56 +0700

    fix:change credential gcp

--- config/google.php
+++ config/google.php
@@ -56,7 +56,7 @@ return [
          * instead of a file path.
          */
 
-        'file' => storage_path('app/' . env('GOOGLE_SERVICE_ACCOUNT_CREDENTIALS')),
+        'file' => storage_path('app/google-service-account.json'),
         // 'file' => env('GOOGLE_SERVICE_ACCOUNT_JSON_LOCATION', ''),
     ],
 
