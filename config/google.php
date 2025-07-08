1a2f410fc3a386dcf8c4d825b86f97505f7f488d
Muhammad Roihan <86415544+roihan165@users.noreply.github.com>
2025-07-08 07:36:13 +0000

    fix: changed ingot credetials

--- config/google.php
+++ config/google.php
@@ -56,7 +56,7 @@ return [
          * instead of a file path.
          */
 
-        'file' => env('GOOGLE_SERVICE_ACCOUNT_JSON_LOCATION','/workspaces/ppsi-app/storage/app/google-service-account.json')
+        'file' => env('GOOGLE_SERVICE_ACCOUNT_CREDENTIALS','/workspaces/ppsi-app/storage/app/google-service-account.json')
         // 'file' => env('GOOGLE_SERVICE_ACCOUNT_JSON_LOCATION', ''),
     ],
 
